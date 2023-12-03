e92d58182d037e2398b0d7d67040b3c2eb2f6833
Sxmourai <samourai_x@outlook.fr>
2023-12-03 18:53:20 +0100

    Finished part 1 of day 2 of adc

--- 2023/02.py
+++ 2023/02.py
@@ -1,12 +1,31 @@
-with open("02.input", "r") as f:
+import sys
+file_name = "02"
+if len(sys.argv)>1 and sys.argv[1] == "test":
+    file_name += "_test"
+with open(f"{file_name}.input", "r") as f:
     c = f.readlines()
 
-
+maxs = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+sum_ids = 0
 for game_id,game in enumerate(c):
     game_id +=1 # Starts at 1
     takes = game.split(": ")[-1] # Takes second part (the cubes)
-    print("Game",game_id,":")
+
     for load in takes.split(";"):
         for take in load.split(","):
             amount,color = take.lstrip(" ").split(" ")
-            print(f"\ttook {amount} cubes colored {color}")
+            amount = int(amount)
+            color = color.strip() # remove potential \n
+            if amount > maxs[color]:
+                print(f"Game {game_id} is impossible, too much {color} ({amount} > {maxs[color]})")
+                break
+        else: # continue breaking if broke up ^^
+            continue
+        break
+    else: # No break, so add id
+        sum_ids += game_id
+print(sum_ids) # 2256
