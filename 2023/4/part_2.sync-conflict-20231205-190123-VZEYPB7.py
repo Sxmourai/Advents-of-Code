bf260ffb4ce69f7698d2fceabb4e97a4d12a0eda
Sxmourai <samourai_x@outlook.fr>
2023-12-05 19:16:44 +0100

    Finished day 4

--- 2023/4/part_2.sync-conflict-20231205-190123-VZEYPB7.py
+++ 2023/4/part_2.sync-conflict-20231205-190123-VZEYPB7.py
@@ -13,37 +13,35 @@ def get_points(card_id):
             points += 1
     return points
 
+def get_nums(card_id):
+    line = r[card_id-1]
+    line = line[8:]
+    winning, ours = line.split(" | ")
+    winning = list(map(lambda x: int(x), filter(lambda x: x.strip()!='', winning.split(" "))))
+    ours = list(map(lambda x: int(x), filter(lambda x: x.strip()!='', ours.split(" "))))
+    return winning, ours
+
 def get_copies(card_id):
     copies = 0
     winning, ours = get_nums(card_id)
     for our in ours:
         if our in winning:
             copies += 1
-    s[0]+=1
-    if cards.get(card_id):
-        cards[card_id] += 1
-    else:
-        cards[card_id] = 1
-    for copy in range(card_id+1, card_id+copies+1):
-        # print(copy)
-        get_copies(copy)
-    #     s_card_points+=get_points(copy)
-    # return s_card_points
+    s_card_points = 1
+    for copy in range(card_id+1, card_id+copies):
+        s_card_points+=get_copies(copy)
+    if card_id == 1:
+        print(card_id, s_card_points, copies)
+        
+        print(s_card_points)
+    return s_card_points
 
-def get_nums(card_id):
-    line = r[card_id-1]
-    line = line[10:]
-    winning, ours = line.split(" | ")
-    winning = list(map(lambda x: int(x), filter(lambda x: x.strip()!='', winning.split(" "))))
-    ours = list(map(lambda x: int(x), filter(lambda x: x.strip()!='', ours.split(" "))))
-    return winning, ours
-
-s = [0,]
-cards = {}
+s = 0
 for i,line in enumerate(r):
     i+=1
-    get_copies(i)
-    # s += get_copies(i)
+    # print(s)
+    copies = get_copies(i)
+    # print(i,copies)
+    s += copies
 
-print("Result is: ", s[0])
-print(cards)
+print("Result is: ", s)
